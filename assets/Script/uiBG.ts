9a559f0ee07c1df4f2bc048b07c79533e4afa0db
Nevermoreness <164292041@qq.com>
2020-07-15 17:45:30 +0800

    Initial commit

---
+++ assets/Script/uiBG.ts
@@ -0,0 +1,33 @@
+const { ccclass, property } = cc._decorator;
+
+@ccclass
+export default class uiBG extends cc.Component {
+
+    @property(cc.Node) m_viewUI: cc.Node = null;
+    viewUI: cc.Animation = null;
+
+    onLoad() {
+        this.viewUI = this.m_viewUI.getComponent(cc.Animation);
+        this.m_viewUI.active = false;
+    }
+
+
+    start() { }
+
+    show() {
+        this.node.active = true;
+    }
+    hide() {
+        this.node.active = false;
+    }
+
+    onClickScaleToShow() {
+        this.m_viewUI.active = true;
+        this.viewUI.play('ScaleToShow');
+    }
+
+    onClickScaleToHide() {
+        this.viewUI.play('ScaleToHide');
+    }
+    // update (dt) {}
+}
